ea91961c37ba3f4a57f3545d1098326ed2321b58
psangwon62 <psangwon62@gmail.com>
2023-10-15 19:27:02 +0900

    [FIX] #136 - 비디오 프레임워크 올라가지 않는 문제 해결

---
+++ Aespa/Aespa.h
@@ -0,0 +1,18 @@
+//
+//  Aespa.h
+//  Aespa
+//
+//  Created by 박상원 on 10/15/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Aespa.
+FOUNDATION_EXPORT double AespaVersionNumber;
+
+//! Project version string for Aespa.
+FOUNDATION_EXPORT const unsigned char AespaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Aespa/PublicHeader.h>
+
+
