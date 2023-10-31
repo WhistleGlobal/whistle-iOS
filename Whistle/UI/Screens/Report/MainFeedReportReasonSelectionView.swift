4bd2dc73bf0cd755505e52ddc764680b96e20586
dayjack <frostjanna12@gmail.com>
2023-10-31 15:47:15 +0900

    [REFACTOR] #188 신고뷰 Divider 오른쪽에 차도록 적용

--- Whistle/UI/Screens/Report/MainFeedReportReasonSelectionView.swift
+++ Whistle/UI/Screens/Report/MainFeedReportReasonSelectionView.swift
@@ -22,17 +22,20 @@ struct MainFeedReportReasonSelectionView: View {
     NavigationStack {
       VStack(spacing: 0) {
         Divider().frame(width: UIScreen.width)
-        Text("이 콘텐츠를 신고하는 이유는 무엇인가요?")
-          .fontSystem(fontDesignSystem: .subtitle2_KO)
-          .foregroundColor(.LabelColor_Primary)
-          .frame(maxWidth: .infinity, alignment: .leading)
-          .padding(.top, 16)
-          .padding(.bottom, 4)
-        Text("지식재산권 침해를 신고하는 경우를 제외하고 회원님의 신고는 익명으로 처리됩니다. 누군가 위급한 상황에 있다고 생각된다면 즉시 현지 응급 서비스 기관에 연락하시기 바랍니다.")
-          .lineLimit(5)
-          .fontSystem(fontDesignSystem: .caption_KO_Regular)
-          .foregroundColor(.LabelColor_Secondary)
-          .padding(.bottom, 16)
+        Group {
+          Text("이 콘텐츠를 신고하는 이유는 무엇인가요?")
+            .fontSystem(fontDesignSystem: .subtitle2_KO)
+            .foregroundColor(.LabelColor_Primary)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.top, 16)
+            .padding(.bottom, 4)
+          Text("지식재산권 침해를 신고하는 경우를 제외하고 회원님의 신고는 익명으로 처리됩니다. 누군가 위급한 상황에 있다고 생각된다면 즉시 현지 응급 서비스 기관에 연락하시기 바랍니다.")
+            .lineLimit(5)
+            .fontSystem(fontDesignSystem: .caption_KO_Regular)
+            .foregroundColor(.LabelColor_Secondary)
+            .padding(.bottom, 16)
+        }
+        .padding(.horizontal, 16)
         Divider().frame(width: UIScreen.width)
         ForEach(ContentReportReason.allCases, id: \.self) { reason in
           NavigationLink {
@@ -41,14 +44,13 @@ struct MainFeedReportReasonSelectionView: View {
               reportReason: reason.numericValue,
               contentId: contentId,
               uesrId: userId)
-
           } label: {
             reportRow(text: reason.rawValue)
           }
+          Divider().frame(height: 0.5).padding(.leading, 16).foregroundColor(.Disable_Placeholder)
         }
         Spacer()
       }
-      .padding(.horizontal, 16)
       .background(Color.reactiveBackground)
       .navigationBarBackButtonHidden()
       .navigationTitle("신고")
@@ -85,5 +87,6 @@ extension MainFeedReportReasonSelectionView {
         .foregroundColor(.Disable_Placeholder)
     }
     .frame(height: 56)
+    .padding(.horizontal, 16)
   }
 }
