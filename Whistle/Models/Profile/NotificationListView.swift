8ba8ae3ee63dda2e500cd8440f58298dc6130939
dayjack <frostjanna12@gmail.com>
2023-11-06 12:31:39 +0900

    [FEAT] #202 팔로잉 알림 화면이동 등 구현 완

--- Whistle/Models/Profile/NotificationListView.swift
+++ Whistle/Models/Profile/NotificationListView.swift
@@ -13,16 +13,26 @@ import SwiftUI
 struct NotificationListView: View {
   @StateObject var apiViewModel = APIViewModel.shared
   @State var newId = UUID()
-  let seprator = SeprateNickAndContent()
 
   var body: some View {
     ScrollView {
       VStack(spacing: 0) {
         Divider().foregroundColor(.Disable_Placeholder)
 
-        ForEach(Array(apiViewModel.notiList.enumerated()), id: \.element) { _, noti in
+        ForEach(
+          apiViewModel.notiList.filter { noti in noti.senderID != apiViewModel.myProfile.userId },
+          id: \.self)
+        { noti in
           if noti.contentID == nil {
-            contentFollowNotiRow(noti)
+            NavigationLink {
+              ProfileView(
+                profileType: .member,
+                isFirstProfileLoaded: .constant(true),
+                userId: noti.senderID)
+            } label: {
+              contentFollowNotiRow(noti)
+            }
+            .id(UUID())
           } else {
             contentWhistleNotiRow(noti)
           }
@@ -31,6 +41,7 @@ struct NotificationListView: View {
             .padding(.leading, 74)
             .foregroundColor(.Disable_Placeholder)
         }
+
         .onReceive(apiViewModel.publisher) { id in
           newId = id
         }
@@ -42,21 +53,6 @@ struct NotificationListView: View {
     .toolbarRole(.editor)
     .onAppear {
       apiViewModel.requestNotiList()
-      let dateString = "2022-09-02T05:19:19.000Z"
-      let dateFormatter = DateFormatter()
-      dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
-      dateFormatter.timeZone = TimeZone(abbreviation: "UTC") // 시간대를 UTC로 설정
-
-      if let date = dateFormatter.date(from: dateString) {
-        print(date) // 날짜가 정상적으로 변환됩니다.
-        let testString = Date.timeAgoSinceDate(date)
-        print(testString)
-      } else {
-        print("날짜 변환에 실패했습니다.")
-      }
-      seprator.seprateNickAndContent()
-      print("Nickname: \(seprator.nickname)")
-      print("Content: \(seprator.content)")
     }
   }
 }
@@ -183,23 +179,6 @@ extension NotificationListView {
                 }
               }
           }
-    }
-  }
-}
-
-// MARK: - SeprateNickAndContent
-
-class SeprateNickAndContent {
-  var nickname = ""
-  var content = ""
-  var givenText = "Whistle님이 회원님의 게시물에 휘슬을 보냈습니다"
-
-  func seprateNickAndContent() {
-    if let range = givenText.range(of: "님이") {
-      nickname = String(givenText[..<range.lowerBound])
-      content = String(givenText[range.lowerBound...])
-    } else {
-      print("텍스트 분리에 문제가 있습니다.")
     }
   }
 }
