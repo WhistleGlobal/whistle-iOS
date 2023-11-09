de6a6d0d230d1731f21da11d40c0b1816940dad3
dayjack <frostjanna12@gmail.com>
2023-11-09 20:52:28 +0900

    [FEAT] #221 첫 검색창 UI 수정

---
+++ Whistle/UI/Screens/Search/SearchResultView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResultView.swift
+//  Whistle
+//
+//  Created by ChoiYujin on 11/9/23.
+//
+
+import SwiftUI
+
+struct SearchResultView: View {
+  var body: some View {
+    VStack(spacing: 0) {
+      Text("Hello, World!")
+    }
+  }
+}
+
+#Preview {
+  SearchResultView()
+}
